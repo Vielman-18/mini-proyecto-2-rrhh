79ec5bf9f92963b1e3991d29addbcf34a717d005
Marvin204 <mzepedaq@miumg.edu.gt>
2026-04-15 23:19:14 -0600

    US-07: creación de módulo, service y controller de empleados con eliminación

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmpleadosModule } from './empleados/empleados.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [EmpleadosModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
