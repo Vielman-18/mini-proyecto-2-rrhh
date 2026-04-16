a31675790997b9ed956016db3551fe318f8a81a5
Marvin204 <mzepedaq@miumg.edu.gt>
2026-04-16 15:06:24 -0600

    US-08: implementar cambio de estado de empleado (activo, suspendido, retirado)

--- Backend/src/empleados/empleados.service.ts
+++ Backend/src/empleados/empleados.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -20,6 +20,34 @@ export class EmpleadosService {
 
     return {
       message: 'Empleado eliminado correctamente',
+    };
+  }
+
+  async cambiarEstado(id: number, estado: string) {
+    const estadosValidos = ['activo', 'suspendido', 'retirado'];
+
+    if (!estadosValidos.includes(estado)) {
+      throw new BadRequestException(
+        'Estado inválido. Solo se permite: activo, suspendido o retirado',
+      );
+    }
+
+    const empleado = await this.prisma.empleados.findUnique({
+      where: { id },
+    });
+
+    if (!empleado) {
+      throw new NotFoundException(`Empleado con id ${id} no existe`);
+    }
+
+    const empleadoActualizado = await this.prisma.empleados.update({
+      where: { id },
+      data: { estado },
+    });
+
+    return {
+      message: 'Estado del empleado actualizado correctamente',
+      data: empleadoActualizado,
     };
   }
 }
