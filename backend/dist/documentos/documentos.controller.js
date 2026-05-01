9541aa2bf75512ae18fb8367f78b48cfd407f27e
Marvin204 <Mzepedaq@miumg.edu.gt>
2026-04-30 23:21:01 -0600

    implementación de módulo documentos (upload PDF, DTO, controller, service, Prisma)

--- backend/dist/documentos/documentos.controller.js
+++ backend/dist/documentos/documentos.controller.js
@@ -19,6 +19,7 @@ const multer_1 = require("multer");
 const path_1 = require("path");
 const swagger_1 = require("@nestjs/swagger");
 const documentos_service_1 = require("./documentos.service");
+const crear_documento_dto_1 = require("./dto/crear-documento.dto");
 let DocumentosController = class DocumentosController {
     documentosService;
     constructor(documentosService) {
@@ -26,6 +27,9 @@ let DocumentosController = class DocumentosController {
     }
     async subirArchivo(file, body) {
         return await this.documentosService.guardarDocumento(file, body);
+    }
+    async listarPorEmpleado(id) {
+        return await this.documentosService.listarPorEmpleado(id);
     }
 };
 exports.DocumentosController = DocumentosController;
@@ -37,22 +41,24 @@ __decorate([
             type: 'object',
             properties: {
                 archivo: { type: 'string', format: 'binary' },
-                tipo_documento: { type: 'string', example: 'DPI' },
                 empleado_id: { type: 'number', example: 1 },
+                usuario_id: { type: 'number', example: 1 },
+                tipo_documento: { type: 'string', example: 'DPI' },
             },
+            required: ['archivo', 'empleado_id', 'usuario_id', 'tipo_documento'],
         },
     }),
     (0, common_1.UseInterceptors)((0, platform_express_1.FileInterceptor)('archivo', {
         storage: (0, multer_1.diskStorage)({
             destination: './uploads',
             filename: (req, file, callback) => {
-                const nombre = Date.now() + (0, path_1.extname)(file.originalname);
-                callback(null, nombre);
+                const nombreArchivo = `${Date.now()}${(0, path_1.extname)(file.originalname)}`;
+                callback(null, nombreArchivo);
             },
         }),
         fileFilter: (req, file, callback) => {
             if (file.mimetype !== 'application/pdf') {
-                return callback(new Error('Solo se permiten PDFs'), false);
+                return callback(new Error('Solo se permiten archivos PDF'), false);
             }
             callback(null, true);
         },
@@ -60,10 +66,18 @@ __decorate([
     __param(0, (0, common_1.UploadedFile)()),
     __param(1, (0, common_1.Body)()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Object, Object]),
+    __metadata("design:paramtypes", [Object, crear_documento_dto_1.CrearDocumentoDto]),
     __metadata("design:returntype", Promise)
 ], DocumentosController.prototype, "subirArchivo", null);
+__decorate([
+    (0, common_1.Get)('empleado/:id'),
+    __param(0, (0, common_1.Param)('id', common_1.ParseIntPipe)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], DocumentosController.prototype, "listarPorEmpleado", null);
 exports.DocumentosController = DocumentosController = __decorate([
+    (0, swagger_1.ApiTags)('Documentos'),
     (0, common_1.Controller)('documentos'),
     __metadata("design:paramtypes", [documentos_service_1.DocumentosService])
 ], DocumentosController);
