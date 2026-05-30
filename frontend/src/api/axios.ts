e5543ebf3d96e258006b76f1e14f693ef081efcb
Vielman <vvasquezr3@miumg.edu.gt>
2026-05-30 08:30:16 -0600

    soluciones neon

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://rrhh-backend-jr1g.onrender.com';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://rrhh-nomina-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
